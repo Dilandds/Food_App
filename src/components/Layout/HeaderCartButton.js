8085568b9287fd73f065c7e016fa62d8a086c2ee
Dilandds <e17379@eng.pdn.ac.lk>
2023-03-19 12:42:09 +0600

    button popup functionality for cart added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,36 @@
 import CartIcon from "../Cart/CartIcon";
 import design from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import cartcontext from "../../store/cart-content";
 const CartButton = (props) => {
+  const [btnIsHighlishted, setButtonHighlighted] = useState(false);
+
   const ctx = useContext(cartcontext);
+  const { items } = ctx;
+
   const totalCartItems = ctx.items.reduce((count, item) => {
     return count + item.count;
   }, 0);
 
+  const buttonClasses = `${design.button} ${
+    btnIsHighlishted ? design.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length > 0) {
+      setButtonHighlighted(true);
+      console.log("here");
+      const timer = setTimeout(() => {
+        setButtonHighlighted(false);
+      }, 300);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={design.button} onClick={props.onCartClick}>
+    <button className={buttonClasses} onClick={props.onCartClick}>
       <span className={design.icon}>
         <CartIcon></CartIcon>
       </span>
